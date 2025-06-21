9afbd4bca8b5e348984ae9e2b1aa48348291ae71
Ryo SUETSUGU <suetsugu.r@gmail.com>
2025-06-21 17:26:18 +0900

    use cobra

--- cmd/main.go
+++ cmd/main.go
@@ -4,32 +4,65 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/spf13/cobra"
 	"github.com/suer/tf-arm/internal/analyzer"
 	"github.com/suer/tf-arm/internal/parser"
 	"github.com/suer/tf-arm/internal/reporter"
 )
 
-func main() {
-	fmt.Println("tf-arm: Terraform State ARM64 Analyzer")
+var version = "1.0.0"
+var showVersion bool
 
-	if len(os.Args) < 2 {
-		printUsage()
+var rootCmd = &cobra.Command{
+	Use:   "tf-arm [state-file]",
+	Short: "Terraform State ARM64 Analyzer",
+	Long: `tf-arm analyzes Terraform state files to identify AWS resources
+that can be migrated to ARM64 architecture for cost optimization.
+
+Supported AWS Services:
+  - Amazon EC2 (aws_instance, aws_launch_template)
+  - AWS Lambda (aws_lambda_function)
+  - Amazon ECS (aws_ecs_task_definition, aws_ecs_service)
+  - Amazon RDS (aws_db_instance, aws_rds_cluster)
+  - Amazon ElastiCache (aws_elasticache_cluster)
+  - Amazon MemoryDB (aws_memorydb_cluster)
+  - Amazon EKS (aws_eks_node_group)
+  - Amazon EMR (aws_emr_cluster, aws_emrserverless_application)
+  - Amazon OpenSearch (aws_opensearch_domain)
+  - Amazon MSK (aws_msk_cluster)
+  - AWS CodeBuild (aws_codebuild_project)
+  - Amazon SageMaker (aws_sagemaker_endpoint_configuration)
+  - Amazon GameLift (aws_gamelift_fleet)`,
+	Args: cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			fmt.Printf("tf-arm version %s\n", version)
+			return
+		}
+
+		if len(args) == 0 {
+			cmd.Help()
+			return
+		}
+
+		stateFile := args[0]
+		analyzeStateFile(stateFile)
+	},
+}
+
+func init() {
+	rootCmd.Flags().BoolVarP(&showVersion, "version", "v", false, "Show version information")
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+}
 
-	// Handle help flags
-	if os.Args[1] == "--help" || os.Args[1] == "-h" {
-		printUsage()
-		os.Exit(0)
-	}
-
-	// Handle version flag
-	if os.Args[1] == "--version" || os.Args[1] == "-v" {
-		fmt.Println("tf-arm version 1.0.0")
-		os.Exit(0)
-	}
-
-	stateFile := os.Args[1]
+func analyzeStateFile(stateFile string) {
+	fmt.Println("tf-arm: Terraform State ARM64 Analyzer")
 	fmt.Printf("Analyzing Terraform state file: %s\n", stateFile)
 	fmt.Println("")
 
@@ -63,37 +96,4 @@ func main() {
 	}
 
 	rep.PrintSummary(totalAnalyzedCount, arm64CompatibleCount)
-}
-
-func printUsage() {
-	fmt.Println("Usage: tf-arm [OPTIONS] <terraform-state-file>")
-	fmt.Println("")
-	fmt.Println("This tool analyzes Terraform state files to identify AWS resources")
-	fmt.Println("that can be migrated to ARM64 architecture for cost optimization.")
-	fmt.Println("")
-	fmt.Println("Arguments:")
-	fmt.Println("  <terraform-state-file>    Path to the Terraform state file to analyze")
-	fmt.Println("")
-	fmt.Println("Options:")
-	fmt.Println("  -h, --help               Show this help message and exit")
-	fmt.Println("  -v, --version            Show version information and exit")
-	fmt.Println("")
-	fmt.Println("Examples:")
-	fmt.Println("  tf-arm terraform.tfstate")
-	fmt.Println("  tf-arm infrastructure.tfstate")
-	fmt.Println("")
-	fmt.Println("Supported AWS Services:")
-	fmt.Println("  - Amazon EC2 (aws_instance, aws_launch_template)")
-	fmt.Println("  - AWS Lambda (aws_lambda_function)")
-	fmt.Println("  - Amazon ECS (aws_ecs_task_definition, aws_ecs_service)")
-	fmt.Println("  - Amazon RDS (aws_db_instance, aws_rds_cluster)")
-	fmt.Println("  - Amazon ElastiCache (aws_elasticache_cluster)")
-	fmt.Println("  - Amazon MemoryDB (aws_memorydb_cluster)")
-	fmt.Println("  - Amazon EKS (aws_eks_node_group)")
-	fmt.Println("  - Amazon EMR (aws_emr_cluster, aws_emrserverless_application)")
-	fmt.Println("  - Amazon OpenSearch (aws_opensearch_domain)")
-	fmt.Println("  - Amazon MSK (aws_msk_cluster)")
-	fmt.Println("  - AWS CodeBuild (aws_codebuild_project)")
-	fmt.Println("  - Amazon SageMaker (aws_sagemaker_endpoint_configuration)")
-	fmt.Println("  - Amazon GameLift (aws_gamelift_fleet)")
 }
